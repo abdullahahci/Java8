fa48668382ea7095020f4513d2301a004805b09b
68752560 <abdullah.ahci@gmail.com>
2020-03-07 19:04:48 +0300

    First commit

---
+++ src/com/ahci/fundamentals/Lambda.java
@@ -0,0 +1,15 @@
+package com.ahci.fundamentals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Lambda {
+	public static void main(String[] args) {
+		Runnable runnable2 = () -> System.out.println("1.8");
+        runnable2.run();
+        
+	}
+}
